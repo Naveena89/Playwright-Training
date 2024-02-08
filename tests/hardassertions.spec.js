8fb895f2c6e158253058d3cc6e0d33accb539317
Naveena89 <naveena3489@gmail.com>
2024-02-08 18:21:20 +0530

    Assignment

--- tests/hardassertions.spec.js
+++ tests/hardassertions.spec.js
@@ -1,13 +1,13 @@
-const{test,expect}=require('@playwright/test')
-    test('Assertions',async({page})=>{
+const {test,expect}=require('@playwright/test')
+test('assertions',async({page})=>{
         await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
         await expect(page).toHaveTitle('OrangeHRM');
-        await expect.hard(page.getByText('Username', { exact: true })).toBeVisible(); //hard assertion
-
+        await expect(page.getByText('Username', { exact: true })).toBeVisible(); //soft assertion
         await page.locator("//input[@name='username']").click();
         await page.locator("//input[@name='username']").fill('Admin');
         await page.locator("//input[@name='password']").click();
         await page.locator("//input[@name='password']").fill('admin123');
+        //await page.hard.click("//button[@type='submit']")
         await page.click("//button[@type='submit']")
         await page.close();
     }
